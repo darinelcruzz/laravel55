454c58d9cc66e0088418553342c5118d4bc18062
Darinel <darinelcruzz@gmail.com>
2017-11-15 15:24:09 -0600

    Helper function usesas added

---
+++ app/Helpers.php
@@ -0,0 +1,9 @@
+<?php
+
+function usesas($ctrl, $fun, $as = null)
+{
+    if ($as) {
+        return ['uses' => "$ctrl@$fun", 'as' => $as];
+    }
+    return ['uses' => "$ctrl@$fun", 'as' => $fun];
+}
